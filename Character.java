d2a6e03a3e47f20fda8ae214ecfb1a351a70333c
jamisem0929 <ethan.jamison@washburn.edu>
2026-08-28 20:41:45 -0500

    initial commit

---
+++ Character.java
@@ -0,0 +1,51 @@
+class Character{
+    //parent variables
+    private String name;
+    private int health;
+    private int level;
+    //parent constructor
+    Character(String name, int health, int level){
+        this.name = name;
+        this.health = health;
+        this.level = level;
+    }
+    void attack(){
+        System.out.printf("Character attacks");
+    }
+    //getter
+     public String getName(){
+        return name;
+    }
+}
+
+class Warrior extends Character{
+    //Warrior variables
+    private int strength;
+    //Warrior constructor
+    Warrior(String name, int health, int level, int strength){
+        super(name , health , level);
+        this.strength = strength;
+        
+    }
+    //Warrior attack 
+    @Override
+    void attack(){
+        System.out.printf("%s attacks with a sword" , this.getName());
+        
+    }
+}
+class Mage extends Character{
+    //Mage variables
+    private int mana;
+    //Mage constructor
+    Mage(String name, int health, int level, int mana){
+        super(name , health , level);
+        this.mana = mana;
+    }
+    //Mage attack
+    @Override
+    void attack(){
+        System.out.printf("%s casts a spell" , this.getName());
+        
+    }
+}
