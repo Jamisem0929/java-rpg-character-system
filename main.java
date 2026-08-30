9dd15f5e46f63d1fbf2a67e414adf5d818788402
jamisem0929 <ethan.jamison@washburn.edu>
2026-08-30 17:05:27 -0500

    Added findHighestHealth() and countAboveHealth() methods

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class Main{
         
         GameCharacter[] characters = {
             new Warrior("John",  100, 1, 150),
-            new Mage("Ethan", 100, 1, 200),
+            new Mage("Ethan", 200, 1, 200),
             new Warrior("Asta" , 100, 1, 200)
         };
         for (GameCharacter character : characters) {
@@ -19,7 +19,9 @@ public class Main{
         else {
             System.out.println("Character not found");
         }
-    
+        
+        GameCharacter highest = findHighestHealth(characters);
+        System.out.println(highest.getName());
     }
     public static GameCharacter findCharacter(GameCharacter[] characters, String targetName){
         for (GameCharacter character: characters) {
@@ -30,5 +32,28 @@ public class Main{
             
         }
         return null;
+    }
+    public static GameCharacter findHighestHealth(GameCharacter[] characters){
+            int highestHealth = characters[0].getHealth();
+            int highestIndex = 0;
+        for (int i = 1; i < characters.length; i++){
+             if (characters[i].getHealth() > highestHealth){
+             
+             highestHealth = characters[i].getHealth();
+             highestIndex = i;
+            
+            }
+        }
+        return characters[highestIndex];
+        
+    }
+    public static int countAboveHealth(GameCharacter[] characters, int amount){
+        int count = 0;
+        for (int i = 0; i < characters.length; i++){
+            if (characters[i].getHealth() > amount){
+                count++;
+            }
+        }
+        return count;
     }
 }
